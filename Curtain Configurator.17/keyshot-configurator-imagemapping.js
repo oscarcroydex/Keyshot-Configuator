a8ac80672e35063e2b6002ae79025aa2bc1d0f4a
oscarcroydex <oscar.robinson@croydex.co.uk>
2024-12-04 13:50:10 +0000

    Add files via upload

---
+++ Curtain Configurator.17/keyshot-configurator-imagemapping.js
@@ -0,0 +1,48 @@
+imagemappingdata =
+[
+    {
+        "image": "Curtain Configurator.17.1.png",
+        "modelsets": [
+            877016375
+        ],
+        "multimaterials": [
+            "7+1"
+        ]
+    },
+    {
+        "image": "Curtain Configurator.17.2.png",
+        "modelsets": [
+            877016375
+        ],
+        "multimaterials": [
+            "7+2"
+        ]
+    },
+    {
+        "image": "Curtain Configurator.17.3.png",
+        "modelsets": [
+            877016375
+        ],
+        "multimaterials": [
+            "7+3"
+        ]
+    },
+    {
+        "image": "Curtain Configurator.17.4.png",
+        "modelsets": [
+            877016375
+        ],
+        "multimaterials": [
+            "7+5"
+        ]
+    },
+    {
+        "image": "Curtain Configurator.17.5.png",
+        "modelsets": [
+            877016375
+        ],
+        "multimaterials": [
+            "7+6"
+        ]
+    }
+];
---
+++ Curtain Configurator.17/keyshot-configurator-modeldata.js
@@ -0,0 +1,58 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "icon": "material%20set%201%201.png",
+                    "multimaterials": [
+                        "7+1"
+                    ],
+                    "name": "Material set 1 1"
+                },
+                {
+                    "icon": "material%20set%201%202.png",
+                    "multimaterials": [
+                        "7+2"
+                    ],
+                    "name": "Material set 1 2"
+                },
+                {
+                    "icon": "material%20set%201%203.png",
+                    "multimaterials": [
+                        "7+3"
+                    ],
+                    "name": "Material set 1 3"
+                },
+                {
+                    "icon": "material%20set%201%204.png",
+                    "multimaterials": [
+                        "7+5"
+                    ],
+                    "name": "Material set 1 4"
+                },
+                {
+                    "checked": true,
+                    "icon": "material%20set%201%205.png",
+                    "multimaterials": [
+                        "7+6"
+                    ],
+                    "name": "Material set 1 5"
+                }
+            ],
+            "name": "Material set 1"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "icon": "curtain%20base.png",
+            "materialgroups": [
+                "Material set 1"
+            ],
+            "modelset": 877016375,
+            "name": "CURTAIN BASE"
+        }
+    ],
+    "studios": []
+};
